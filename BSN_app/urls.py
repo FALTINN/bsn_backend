e8a6165e5967e75f783d73566ffdc4ff92171150
Timurka <tima_abdrashitov.00@mail.ru>
2024-12-19 23:19:14 +0300

    html added

--- BSN_app/urls.py
+++ BSN_app/urls.py
@@ -1,10 +1,15 @@
 from django.urls import path
 
-from BSN_app.views import MainView, CatalogView, StoreView
+from BSN_app.views import MainView
 
 app_name = 'BSN_app'
 urlpatterns = [
     path('', MainView.as_view(), name="mainpage"),
-    path('catalog', CatalogView.as_view(), name="catalog-page"),
-    path('store', StoreView.as_view(), name="store-page"),
+    path('rich', MainView.as_view(), name="rich-page"),
+    path('king', MainView.as_view(), name="king-page"),
+    path('builder', MainView.as_view(), name="builder-page"),
+    path('redstoner', MainView.as_view(), name="redstoner-page"),
+    path('creator', MainView.as_view(), name="creator-page"),
+    path('building', MainView.as_view(), name="building-page"),
+    path('event', MainView.as_view(), name="event-page"),
 ]
--- BSN_app/views.py
+++ BSN_app/views.py
@@ -1,14 +1,5 @@
-from django.shortcuts import render
-from django.views import View
 from django.views.generic import TemplateView
-import json
 
 
 class MainView(TemplateView):
-  template_name = "main.html"
-
-class CatalogView(TemplateView):
-  template_name = "catalog.html"
-  
-class StoreView(TemplateView):
-  template_name = "store.html"
+  template_name = "base_vue.html"
